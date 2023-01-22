c71b6776086a9f8f3b94be3b403f4e8745125566
Leonardo Teixeira <leonardot@wswork.com.br>
2023-01-22 11:51:03 -0300

    Spring Websockets POC

---
+++ src/main/kotlin/com/poc/springwebsockets/SpringWebsocketsApplication.kt
@@ -0,0 +1,11 @@
+package com.poc.springwebsockets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebsocketsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebsocketsApplication>(*args)
+}
